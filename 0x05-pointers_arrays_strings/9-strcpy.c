23332a356f960db460f01ae894fabb48ad60f864
Adelayo Gbolahan <111052542+arraycoded12@users.noreply.github.com>
2022-09-19 17:51:48 +0100

    Added Task 6 to 9 project 0x05 C

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,41 +1,21 @@
 #include "main.h"
 
-
-
 /**
- *
- *  * _strcpy - copies string
- *
- *   * @dest: poiter
- *
- *    * @src: poiter
- *
- *     * Return: char
- *
- *      */
-
-
+ * _strcpy - copies string
+ * @dest: pointer
+ * @src: pointer
+ * Return: char
+ */
 
 char *_strcpy(char *dest, char *src)
-
 {
+	int i;
 
-		int i;
+	for (i = 0; *(src + i) != '\0'; i++)
+	{
+		dest[i] = *(src + i);
+	}
+	dest[i] = '\0';
 
-
-
-			for (i = 0; *(src + i) != '\0'; i++)
-
-					{
-
-								dest[i] = *(src + i);
-
-									}
-
-				dest[i] = '\0';
-
-
-
-					return (dest);
-
+	return (dest);
 }
