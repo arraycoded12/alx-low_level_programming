07ee875ad2154516e051c84c2731cf6becf6cd5e
Adelayo Gbolahan <111052542+arraycoded12@users.noreply.github.com>
2022-10-12 14:19:41 +0100

    Added Task 1 to 3 project 0x0F

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,91 +1,46 @@
 #include "3-calc.h"
 
-
-
 /**
+ * main - program that perfroms simple operations
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
- *  * main - program that perfroms simple operations
- *
- *   * @argc: number of arguments
- *
- *    * @argv: array of arguments
- *
- *     *
- *
- *      * Return: Always 0 (Success)
- *
- *       */
-
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char *argv[])
-
 {
+	int arg1, arg2, result;
+	char o;
+	int (*func)(int, int);
 
-		int arg1, arg2, result;
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-			char o;
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[3]);
 
-				int (*func)(int, int);
+	func = get_op_func(argv[2]);
 
+	if (!func)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
+	o = *argv[2];
 
-					if (argc != 4)
+	if ((o == '/' || o == '%') && arg2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
-							{
+	result = func(arg1, arg2);
 
-										printf("Error\n");
+	printf("%d\n", result);
 
-												exit(98);
-
-													}
-
-
-
-						arg1 = atoi(argv[1]);
-
-							arg2 = atoi(argv[3]);
-
-
-
-								func = get_op_func(argv[2]);
-
-
-
-									if (!func)
-
-											{
-
-														printf("Error\n");
-
-																exit(99);
-
-																	}
-
-
-
-										o = *argv[2];
-
-
-
-											if ((o == '/' || o == '%') && arg2 == 0)
-
-													{
-
-																printf("Error\n");
-
-																		exit(100);
-
-																			}
-
-
-
-												result = func(arg1, arg2);
-
-
-
-													printf("%d\n", result);
-
-
-
-														return (0);
-
+	return (0);
 }
